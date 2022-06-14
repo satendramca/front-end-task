3957b0401ccb21966718f9eafe92222b105a2d79
Satendra Kumar <satendramca2016@gmail.com>
2022-06-15 00:10:15 +0530

    final commit

---
+++ src/Components/ShowData.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const ShowData = () => {
+  return (
+    <div>
+      <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            {" "}
+            Todo Title{" "}
+          </label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            className="form-control"
+            id="title"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="desc" className="form-label">
+            {" "}
+            Todo Description{" "}
+          </label>
+          <input
+            type="text"
+            value={desc}
+            onChange={(e) => {
+              setDesc(e.target.value);
+            }}
+            className="form-control"
+            id="desc"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-sm btn-success">
+          {" "}
+          Add Todo{" "}
+        </button>
+    </div>
+  )
+}
+
+export default ShowData
--- src/Components/User.js
+++ src/Components/User.js
@@ -1,16 +1,48 @@
 import React from "react";
 import UserData from "./UserData";
+import { useState, useEffect } from "react";
 
 const User = (props) => {
-    // console.log(props.selectedData[0]);
+    const [userData, setData] = useState([]);
+    const [selectedData, setSelectedData] = useState([{}]);
+
+    const showData = (item)=>{
+      fetch(`https://reqres.in/api/users/${item.id}`)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setSelectedData([result.data]);
+          },
+          (error) => {
+            console.error(error);
+            setData([]);
+          }
+        )
+    }
+    
+    useEffect(() => {
+      fetch('https://reqres.in/api/users')
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setData(result.data);
+          },
+          (error) => {
+            console.error(error);
+            setData([]);
+          }
+        )
+    }, []);
+    
+
   return (
     <div>
-      <h3>{props.selectedData[0].id}</h3>
-      <h3>{props.selectedData[0].email}</h3>
-      <h3>{props.selectedData[0].first_name}</h3>
-      <h3>{props.selectedData[0].last_name}</h3>
-      {props.data.map((data) => {
-        return <UserData data={data} showData={props.showData} />;
+      <h3>{selectedData[0].id}</h3>
+      <h3>{selectedData[0].email}</h3>
+      <h3>{selectedData[0].first_name}</h3>
+      <h3>{selectedData[0].last_name}</h3>
+      {userData.map((data) => {
+        return <UserData key={data.id} data={data} showData={showData} />;
       })}
     </div>
   );
